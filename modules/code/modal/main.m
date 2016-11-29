2fb805541b32115c305f2507a9f6e13dc795a858
John DeVitis <johndevitis@gmail.com>
2016-11-29 11:03:13 -0500

    added modal flexibility comparison

--- modules/code/modal/main.m
+++ modules/code/modal/main.m
@@ -19,13 +19,34 @@ function main()
 	% plot FRF measurement with contribution from each mode
     in = 6; out = 6;
     [hs,hh] = vibsFRF(AA,root,in,out,w);
-    fh = vibsFRFplot(hs,hh,in,out,w);
+    vibsFRFplot(hs,hh,in,out,w);
     
     % plot equivalent measurement in time domain
     %  this is the impulse response function formed form the same residue
     %  matrix
     fs = 200; % sampling frequency [hz]
     l = 1;    % length of time vector (seconds)
-    [hs,h] = vibsIRF(AA,root,in,out,fs,l);
-    fh = vibsIRFplot(hs,h,in,out,fs,l);
+    [hsi,h] = vibsIRF(AA,root,in,out,fs,l);
+    vibsIRFplot(hsi,h,in,out,fs,l);
+    
+    % obligatory phase plot
+    vibsPhaseplot(hs,hh,in,out,w);
+    
+    
+    %% modal flexibility
+    flex = zeros(size(V,1),size(V,1));  % pre-allocate dof flex
+    Ma = 1./Qr;                         % unit ModalA based on unit Qr, based on modal mass=1 (due to normalized eigenvectors)    
+    for ii = 1:size(V,2)                % loop modes for flexibility
+        tmp = (V(:,ii)*V(:,ii).')/(Ma(ii)*root(ii));
+        flex = flex+tmp+conj(tmp);
+    end
+    %  compare displacements from strand7 model and modal model
+    % st7 LSA comparison (unit load at st7 nodeid 94)
+    fvec = zeros(size(V,1),1);
+    fvec(6) = 1;
+    modaldisp = flex*fvec;
+    st7disp = -3.759771e-6;
+    % modal flex percent difference
+    d = (modaldisp(6)-st7disp)/st7disp*100;
+    fprintf('Difference in displacement predictions:\t%2.2f%%\n',d);
 end
